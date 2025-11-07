3d5298234340fbbda3063fd1188bcfa51ea9d43f
moabia1 <mdmoabia81@gmail.com>
2025-11-07 10:26:52 +0530

    Completed !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,8 +16,8 @@ export default function App() {
       {/* FOOTER */}
       <footer className="border-t border-gray-200 bg-white">
         <div className="max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-6 text-sm text-gray-500 text-center">
-          Built for screening •
-          <span className="font-medium text-purple-700">Mock E-Com Cart</span>{" "}
+          Built for screening • Mock 
+          <span className="font-medium text-purple-700">E-Com Cart</span>{" "}
         </div>
       </footer>
 
